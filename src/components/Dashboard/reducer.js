ffb8a9ae5924b3a994369af06a2def4fd16b4461
Lewis Nixon <lewcfn@gmail.com>
2018-01-25 14:21:32 +0100

    install redux and tools and seed the store with data

---
+++ src/components/Dashboard/reducer.js
@@ -0,0 +1,6 @@
+// import { fromJS } from 'immutable';
+import { handleActions } from 'redux-actions';
+
+const dashBoardReducer = handleActions({}, null);
+
+export default dashBoardReducer;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore, combineReducers, compose } from 'redux';
+import { Provider } from 'react-redux';
+import dashboard from './components/Dashboard/reducer';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 import configureIDB from './configureIDB';
 
 
-render(<App />, document.getElementById('root'));
-configureIDB().then(data => {
-  // get data from IDB, now seed your reducer or kick off your application
-  console.log('data is: ', data);
-});
+configureIDB()
+  .then(initialState => {
+    console.log('initialState is: ', initialState);
+    const rootReducer = combineReducers({ dashboard });
+    const middlewares = [];
+
+    if (process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION__) {
+      middlewares.push(window.__REDUX_DEVTOOLS_EXTENSION__());
+    }
+
+    const store = compose(...middlewares)(createStore)(rootReducer, { dashboard: initialState });
+
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      document.getElementById('root')
+    );
+  })
+  .catch(err => {
+    // render(
+    //   <div>Something went wrong returning your saved data {err}</div>,
+    //   document.getElementById('root')
+    // );
+    console.log('Something went wrong returning your saved data: ', err);
+  });
 registerServiceWorker();
