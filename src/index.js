0f36e7c0d1db03bf366e9c8fed9ecd1c0a4c4c2c
lewisnixon <lewcfn@gmail.com>
2018-01-25 13:50:31 +0100

    feat(IndexedDB): Setup IndexedDB and seed with dummy data (#4)
    * configure IndexedDB and seed with dummy data
    
    * add indexedDB to the readme

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import { render } from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import configureIDB from './configureIDB';
+
 
 render(<App />, document.getElementById('root'));
+configureIDB().then(data => {
+  // get data from IDB, now seed your reducer or kick off your application
+  console.log('data is: ', data);
+});
 registerServiceWorker();
